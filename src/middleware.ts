d75c5f67260f535fc7ebbad7ea342c358f682ead
Filip Juszczak <juszczak.filip24@gmail.com>
2024-11-29 13:47:25 +0100

    improve middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,11 +3,13 @@ import ky from "ky";
 import type { Session } from "lucia";
 
 export async function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname.startsWith("/staff/dashboard")) {
+  if (request.nextUrl.pathname === "/panel-pracownika") {
     const requestSessionCookie = request.cookies.get("auth_session");
 
     if (!requestSessionCookie) {
-      return NextResponse.redirect(new URL("/staff/login", request.url));
+      return NextResponse.redirect(
+        new URL("/panel-pracownika/logowanie", request.url)
+      );
     }
 
     const { session } = await ky
@@ -19,35 +21,24 @@ export async function middleware(request: NextRequest) {
       })
       .json<{ session: Session }>();
 
-    if (!session || !session.userId) {
-      return NextResponse.redirect(new URL("/staff/login", request.url));
+    if (session && session.userId) {
+      return NextResponse.redirect(
+        new URL("/panel-pracownika/pulpit", request.url)
+      );
     }
+
+    return NextResponse.redirect(
+      new URL("/panel-pracownika/logowanie", request.url)
+    );
   }
 
-  if (request.nextUrl.pathname.startsWith("/staff/login")) {
-    const requestSessionCookie = request.cookies.get("auth_session");
-
-    if (requestSessionCookie) {
-      const { session } = await ky
-        .post(new URL("/api/auth/employee", request.url), {
-          json: { sessionCookie: requestSessionCookie },
-          headers: {
-            secret: process.env.AUTH_API_SECREY_KEY
-          }
-        })
-        .json<{ session: Session }>();
-
-      if (session) {
-        return NextResponse.redirect(new URL("/staff/dashboard", request.url));
-      }
-    }
-  }
-
-  if (request.nextUrl.pathname.startsWith("/staff/change-password")) {
+  if (request.nextUrl.pathname.startsWith("/panel-pracownika/pulpit")) {
     const requestSessionCookie = request.cookies.get("auth_session");
 
     if (!requestSessionCookie) {
-      return NextResponse.redirect(new URL("/staff/login", request.url));
+      return NextResponse.redirect(
+        new URL("/panel-pracownika/logowanie", request.url)
+      );
     }
 
     const { session } = await ky
@@ -59,19 +50,167 @@ export async function middleware(request: NextRequest) {
       })
       .json<{ session: Session }>();
 
-    if (!session) {
-      return NextResponse.redirect(new URL("/staff/login", request.url));
+    if (session && session.userId) {
+      return NextResponse.next();
     }
+
+    return NextResponse.redirect(
+      new URL("/panel-pracownika/logowanie", request.url)
+    );
   }
+
+  if (request.nextUrl.pathname.startsWith("/panel-pracownika/logowanie")) {
+    const requestSessionCookie = request.cookies.get("auth_session");
+
+    if (!requestSessionCookie) {
+      return NextResponse.next();
+    }
+
+    const { session } = await ky
+      .post(new URL("/api/auth/employee", request.url), {
+        json: { sessionCookie: requestSessionCookie },
+        headers: {
+          secret: process.env.AUTH_API_SECREY_KEY
+        }
+      })
+      .json<{ session: Session }>();
+
+    if (session && session.userId) {
+      return NextResponse.redirect(
+        new URL("/panel-pracownika/pulpit", request.url)
+      );
+    }
+
+    return NextResponse.next();
+  }
+
+  if (request.nextUrl.pathname.startsWith("/panel-pracownika/zmien-haslo")) {
+    const requestSessionCookie = request.cookies.get("auth_session");
+
+    if (!requestSessionCookie) {
+      return NextResponse.redirect(
+        new URL("/panel-pracownika/logowanie", request.url)
+      );
+    }
+
+    const { session } = await ky
+      .post(new URL("/api/auth/employee", request.url), {
+        json: { sessionCookie: requestSessionCookie },
+        headers: {
+          secret: process.env.AUTH_API_SECREY_KEY
+        }
+      })
+      .json<{ session: Session }>();
+
+    if (session) {
+      return NextResponse.next();
+    }
+
+    return NextResponse.redirect(
+      new URL("/panel-pracownika/logowanie", request.url)
+    );
+  }
+
+  if (request.nextUrl.pathname.startsWith("/rejestracja")) {
+    console.log("Middleware for /rejestracja running...");
+    const requestSessionCookie = request.cookies.get("auth_session");
+
+    if (!requestSessionCookie) {
+      return NextResponse.next();
+    }
+
+    const { session } = await ky
+      .post(new URL("/api/auth/user", request.url), {
+        json: { sessionCookie: requestSessionCookie },
+        headers: {
+          secret: process.env.AUTH_API_SECREY_KEY
+        }
+      })
+      .json<{ session: Session }>();
+
+    if (session && session.userId) {
+      return NextResponse.redirect(new URL("/konto", request.url));
+    }
+
+    return NextResponse.next();
+  }
+
+  if (request.nextUrl.pathname.startsWith("/logowanie")) {
+    console.log("Middleware for /logowanie running...");
+    const requestSessionCookie = request.cookies.get("auth_session");
+
+    if (!requestSessionCookie) {
+      return NextResponse.next();
+    }
+
+    const { session } = await ky
+      .post(new URL("/api/auth/user", request.url), {
+        json: { sessionCookie: requestSessionCookie },
+        headers: {
+          secret: process.env.AUTH_API_SECREY_KEY
+        }
+      })
+      .json<{ session: Session }>();
+
+    if (session && session.userId) {
+      return NextResponse.redirect(new URL("/konto", request.url));
+    }
+
+    return NextResponse.next();
+  }
+
+  if (request.nextUrl.pathname.startsWith("/konto")) {
+    console.log("Middleware for /konto running...");
+    const requestSessionCookie = request.cookies.get("auth_session");
+
+    if (!requestSessionCookie) {
+      return NextResponse.redirect(new URL("/logowanie", request.url));
+    }
+
+    const { session } = await ky
+      .post(new URL("/api/auth/user", request.url), {
+        json: { sessionCookie: requestSessionCookie },
+        headers: {
+          secret: process.env.AUTH_API_SECREY_KEY
+        }
+      })
+      .json<{ session: Session }>();
+
+    if (session && session.userId) {
+      return NextResponse.next();
+    }
+
+    return NextResponse.redirect(new URL("/logowanie", request.url));
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
   matcher: [
-    "/rejestracja",
-    "/logowanie",
-    "/zresetuj-haslo",
     {
-      source: "/staff/:path*",
+      source: "/rejestracja",
+      missing: [
+        { type: "header", key: "next-router-prefetch" },
+        { type: "header", key: "purpose", value: "prefetch" }
+      ]
+    },
+    {
+      source: "/logowanie",
+      missing: [
+        { type: "header", key: "next-router-prefetch" },
+        { type: "header", key: "purpose", value: "prefetch" }
+      ]
+    },
+    {
+      source: "/konto/:path*",
+      missing: [
+        { type: "header", key: "next-router-prefetch" },
+        { type: "header", key: "purpose", value: "prefetch" }
+      ]
+    },
+    {
+      source: "/panel-pracownika/:path*",
       missing: [
         { type: "header", key: "next-router-prefetch" },
         { type: "header", key: "purpose", value: "prefetch" }
