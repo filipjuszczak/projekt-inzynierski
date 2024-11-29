323073f7fe6cea6527143da64c941bf022576969
Filip Juszczak <juszczak.filip24@gmail.com>
2024-11-29 13:49:35 +0100

    add constants file

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,26 @@
-export const hashingConfig = {
+import { ScreenFormat, ViewingMode } from "@prisma/client";
+
+export const HASHING_CONFIG = {
   memoryCost: 19456,
   timeCost: 2,
   outputLen: 32,
   parallelism: 1
+};
+
+export const VIEWING_MODE_LABELS = {
+  [ViewingMode.SUBTITLES]: "Oryginalny (napisy)",
+  [ViewingMode.DUBBING]: "Dubbing"
+};
+
+export const SCREEN_FORMAT_LABELS = {
+  [ScreenFormat.TWO_D]: "2D",
+  [ScreenFormat.THREE_D]: "3D",
+  [ScreenFormat.IMAX]: "IMAX"
+};
+
+export const GENRE_LABELS = {
+  0: "Brak ograniczenia wiekowego",
+  12: "12+",
+  15: "15+",
+  18: "18+"
 };
