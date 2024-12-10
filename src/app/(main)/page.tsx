8d07e210896083dbd0d5ffefda145d994d9c2c42
Filip Juszczak <juszczak.filip24@gmail.com>
2024-12-10 14:48:33 +0100

    conditionally render UpcomingMovies section based on availability

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -20,14 +20,16 @@ export default async function Page() {
           <LatestMovies movies={latestMovies} />
         </div>
       </section>
-      <section className="w-full py-12 md:py-24 lg:py-32">
-        <div className="container px-4 md:px-6">
-          <h2 className="mb-8 text-3xl font-bold tracking-tighter sm:text-5xl">
-            Wkrótce...
-          </h2>
-          <UpcomingMovies movies={upcomingMovies} />
-        </div>
-      </section>
+      {upcomingMovies.length > 0 && (
+        <section className="w-full py-12 md:py-24 lg:py-32">
+          <div className="container px-4 md:px-6">
+            <h2 className="mb-8 text-3xl font-bold tracking-tighter sm:text-5xl">
+              Wkrótce...
+            </h2>
+            <UpcomingMovies movies={upcomingMovies} />
+          </div>
+        </section>
+      )}
     </main>
   );
 }
