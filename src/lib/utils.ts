4b04f46799bf4fa09fce00652041e8d4a51d68c2
Filip Juszczak <juszczak.filip24@gmail.com>
2024-12-11 14:07:35 +0100

    add isUserOldEnough function to validate age based on birth date and age restriction

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -211,3 +211,20 @@ export function validateCheckoutValues(values: CheckoutFormValues) {
 
   return success ? data : null;
 }
+
+export function isUserOldEnough(
+  birthDate: Date,
+  ageRestriction: number
+): boolean {
+  const today = new Date();
+
+  let age = today.getFullYear() - birthDate.getFullYear();
+  const monthDiff = today.getMonth() - birthDate.getMonth();
+  const dayDiff = today.getDate() - birthDate.getDate();
+
+  if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
+    age--;
+  }
+
+  return age >= ageRestriction;
+}
