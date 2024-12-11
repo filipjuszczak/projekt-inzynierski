612ed8137e244938812d57843e4157342f5cbddf
Filip Juszczak <juszczak.filip24@gmail.com>
2024-12-11 14:08:57 +0100

    add theme toggle buttons to AuthButtons component

--- src/components/AuthButtons.tsx
+++ src/components/AuthButtons.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import Link from "next/link";
+import { useTheme } from "next-themes";
+import { Monitor, Moon, Sun } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
 import UserMenu from "@/components/UserMenu";
 import { useCheckAuth } from "@/app/(main)/queries";
 
 export default function AuthButtons() {
+  const { setTheme, theme } = useTheme();
   const { data, isPending } = useCheckAuth();
 
   return (
@@ -17,6 +20,34 @@ export default function AuthButtons() {
         <Skeleton className="h-10 w-10 rounded-full" />
       ) : (
         <>
+          {theme === "dark" ? (
+            <Button
+              variant="ghost"
+              size="icon"
+              onClick={() => setTheme("system")}
+            >
+              <Monitor />
+              <span className="sr-only">Zmień motyw na systemowy</span>
+            </Button>
+          ) : theme === "light" ? (
+            <Button
+              variant="ghost"
+              size="icon"
+              onClick={() => setTheme("dark")}
+            >
+              <Moon />
+              <span className="sr-only">Zmień motyw na ciemny</span>
+            </Button>
+          ) : (
+            <Button
+              variant="ghost"
+              size="icon"
+              onClick={() => setTheme("light")}
+            >
+              <Sun />
+              <span className="sr-only">Zmień motyw na jasny</span>
+            </Button>
+          )}
           <Button asChild>
             <Link href="/logowanie">Zaloguj się</Link>
           </Button>
