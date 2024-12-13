3e12f962996d60ce9c63da1f128b71223263dad6
Filip Juszczak <juszczak.filip24@gmail.com>
2024-12-13 15:45:45 +0100

    update Screen component to change background color and add border

--- src/components/Screen.tsx
+++ src/components/Screen.tsx
@@ -1,6 +1,6 @@
 export default function Screen() {
   return (
-    <div className="sticky top-0 z-10 mb-4 flex h-8 w-full items-center justify-center rounded-t-3xl bg-gray-400 sm:mb-8 sm:h-12">
+    <div className="mb-4 flex h-8 w-full items-center justify-center rounded-t-3xl border bg-slate-800 sm:mb-8 sm:h-12">
       <span className="text-xs font-semibold text-white sm:text-sm">Ekran</span>
     </div>
   );
