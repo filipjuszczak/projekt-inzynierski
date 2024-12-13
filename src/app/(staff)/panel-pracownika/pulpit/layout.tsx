f8c7d72c4ab8a1874acae5fb492571b5e364fac8
Filip Juszczak <juszczak.filip24@gmail.com>
2024-12-13 18:26:30 +0100

    update layout to fetch and pass movie data to DashboardSidebar; fix ChangePasswordButton link path

--- src/app/(staff)/panel-pracownika/pulpit/layout.tsx
+++ src/app/(staff)/panel-pracownika/pulpit/layout.tsx
@@ -1,10 +1,13 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import DashboardSidebar from "@/app/(staff)/panel-pracownika/pulpit/DashboardSidebar";
+import DashboardSidebar from "@/components/dashboard/DashboardSidebar";
+import { getMovieList } from "@/app/(staff)/panel-pracownika/pulpit/data";
 
-export default function Layout({ children }: React.PropsWithChildren) {
+export default async function Layout({ children }: React.PropsWithChildren) {
+  const movies = await getMovieList();
+
   return (
     <SidebarProvider>
-      <DashboardSidebar />
+      <DashboardSidebar movies={movies} />
       <div className="flex min-h-screen w-full flex-col overflow-auto p-4">
         <SidebarTrigger className="mb-10" />
         <main className="flex flex-grow flex-col">{children}</main>
--- src/components/dashboard/ChangePasswordButton.tsx
+++ src/components/dashboard/ChangePasswordButton.tsx
@@ -5,7 +5,10 @@ import { DropdownMenuItem } from "@/components/ui/dropdown-menu";
 export default function ChangePasswordButton() {
   return (
     <DropdownMenuItem asChild>
-      <Link href="/panel-pracownika/zmien-haslo" className="cursor-pointer">
+      <Link
+        href="/panel-pracownika/pulpit/zmien-haslo"
+        className="cursor-pointer"
+      >
         {" "}
         <KeyRound />
         Zmień hasło
