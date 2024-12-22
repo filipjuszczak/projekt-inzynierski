83d8812ad6d67126ab0417f5c68580fb5cde5bf9
Filip Juszczak <juszczak.filip24@gmail.com>
2024-12-22 15:54:14 +0100

    remove genres badges skeleton

--- src/components/skeletons/MovieCardSkeleton.tsx
+++ src/components/skeletons/MovieCardSkeleton.tsx
@@ -12,10 +12,7 @@ export default function MovieCardSkeleton() {
       <Skeleton className="aspect-[2/3] w-full" />
       <CardContent className="space-y-2 p-4 pb-10">
         <Skeleton className="h-7 w-24" />
-        <div className="flex max-w-full justify-between gap-2 overflow-hidden">
-          <Skeleton className="h-[22px] w-[90px]" />
-          <Skeleton className="h-[22px] w-14" />
-        </div>
+        <Skeleton className="h-[22px] w-[90px]" />
         <Skeleton className="h-14 w-full" />
       </CardContent>
       <CardFooter className="mt-auto px-4">
