1c7868b7a5daeca49dfff54efc50abcc8995d6cd
Filip Juszczak <juszczak.filip24@gmail.com>
2025-01-10 16:53:24 +0100

    add new hostname configuration for UploadThing in Next.js config

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,11 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "utfs.io",
         pathname: `/f/*`
+      },
+      {
+        protocol: "https",
+        hostname: "*.ufs.sh",
+        pathname: `/f/*`
       }
     ]
   },
